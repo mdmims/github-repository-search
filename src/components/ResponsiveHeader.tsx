57087f58dadf59662ade4c1ee50ad656d1ac18a1
mdmims <mdmims.tech@gmail.com>
2023-05-25 13:49:55 -0400

    refactor header

--- src/components/ResponsiveHeader.tsx
+++ src/components/ResponsiveHeader.tsx
@@ -2,13 +2,9 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
 import GithubIcon from '../assets/github-white.svg'
 import {FormControlLabel, FormGroup, Icon, Switch} from "@mui/material";
 import {useNavigate} from "react-router-dom";
@@ -22,17 +18,13 @@ function ResponsiveAppBar({changeTheme, isDarkTheme}: AppBarProps): JSX.Element 
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const navigate = useNavigate();
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
   const handleCloseNavMenu = () => {
     navigate("/");
     setAnchorElNav(null);
   };
 
-
   return (
+      <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Container maxWidth="xl">
           <Toolbar disableGutters>
@@ -62,17 +54,7 @@ function ResponsiveAppBar({changeTheme, isDarkTheme}: AppBarProps): JSX.Element 
             </Typography>
 
             {/*Handle responsive menu title*/}
-            <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
-              <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleOpenNavMenu}
-                  color="inherit"
-              >
-                <MenuIcon/>
-              </IconButton>
+            <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}, justifyContent: 'center'}}>
               <Menu
                   id="menu-appbar"
                   anchorEl={anchorElNav}
@@ -129,6 +111,7 @@ function ResponsiveAppBar({changeTheme, isDarkTheme}: AppBarProps): JSX.Element 
           </Toolbar>
         </Container>
       </AppBar>
+      </Box>
   );
 }
 
