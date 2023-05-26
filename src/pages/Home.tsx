7b8bd39d05d9eb61d94e97e97a13d4ca2effc008
mdmims <mdmims.tech@gmail.com>
2023-05-25 20:26:17 -0400

    prettier formatting

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,76 +1,85 @@
-import { useState, Fragment } from 'react'
-import {ButtonGroup, Skeleton, Stack} from "@mui/material";
-import {useGetPopularRepositories} from "../hooks/useGetMostPopular.tsx";
+import { useState, Fragment } from "react";
+import { ButtonGroup, Skeleton, Stack } from "@mui/material";
+import { useGetPopularRepositories } from "../hooks/useGetMostPopular.tsx";
 import UserRepository from "../components/UserRepository.tsx";
 import Button from "@mui/material/Button";
 import HelpSearchModal from "../components/HelpSearchModal.tsx";
-import {Language} from "@mui/icons-material";
+import { Language } from "@mui/icons-material";
 
-type Language = 'typescript' | 'javascript' | 'python' | 'golang'
+type Language = "typescript" | "javascript" | "python" | "golang";
 
 const Home = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState<Language>('typescript')
-  const { isLoading, data} = useGetPopularRepositories(selectedLanguage)
-  const languageOptions: Language[] = ["typescript", "javascript", "python", "golang"]
+  const [selectedLanguage, setSelectedLanguage] =
+    useState<Language>("typescript");
+  const { isLoading, data } = useGetPopularRepositories(selectedLanguage);
+  const languageOptions: Language[] = [
+    "typescript",
+    "javascript",
+    "python",
+    "golang",
+  ];
 
-  const displaySkeleton = (): JSX.Element[] => (
-      new Array(5).map(item =>
-          <Skeleton key={item} variant="rectangular" width={500} height={150} />
-      )
-  )
+  const displaySkeleton = (): JSX.Element[] =>
+    new Array(5).map((item) => (
+      <Skeleton key={item} variant="rectangular" width={500} height={150} />
+    ));
 
   const displayRepositories = (): JSX.Element => {
     return (
-        <Fragment>
-          {data && data.map((repo, index) => (
-              <UserRepository key={index} {...repo} />
-          ))}
-        </Fragment>
-    )
-  }
+      <Fragment>
+        {data &&
+          data.map((repo, index) => <UserRepository key={index} {...repo} />)}
+      </Fragment>
+    );
+  };
 
   const handleLanguageClick = (lang: Language) => {
-    setSelectedLanguage(lang)
-  }
+    setSelectedLanguage(lang);
+  };
 
   const renderLanguageButtons = () => {
-      return (
-          <ButtonGroup sx={{display: "flex", flexDirection: "row", alignItems: "center", mb: "3 rem"}}>
-            {languageOptions.map((option, index) => (
-                <Button
-                  key={index}
-                  variant="contained"
-                  onClick={() => handleLanguageClick(option)}
-                  sx={{
-                    backgroundColor: selectedLanguage === option ? '#335789' : '#B8B8B8',
-                    '&:hover': {
-                      backgroundColor: '#505050',
-                    },
-                }}
-                >
-                  {option}
-                </Button>
-            ))}
-          </ButtonGroup>
-      )
-  }
+    return (
+      <ButtonGroup
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          mb: "3 rem",
+        }}
+      >
+        {languageOptions.map((option, index) => (
+          <Button
+            key={index}
+            variant="contained"
+            onClick={() => handleLanguageClick(option)}
+            sx={{
+              backgroundColor:
+                selectedLanguage === option ? "#335789" : "#B8B8B8",
+              "&:hover": {
+                backgroundColor: "#505050",
+              },
+            }}
+          >
+            {option}
+          </Button>
+        ))}
+      </ButtonGroup>
+    );
+  };
 
   return (
-      <Stack
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          spacing={0.5}
-          sx={{mt: '2rem'}}
-      >
-        <HelpSearchModal />
-        <h3>Most popular repositories by Language</h3>
-        {renderLanguageButtons()}
-        {isLoading ? (
-            displaySkeleton()
-        ) : displayRepositories()
-        }
-      </Stack>
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={0.5}
+      sx={{ mt: "2rem" }}
+    >
+      <HelpSearchModal />
+      <h3>Most popular repositories by Language</h3>
+      {renderLanguageButtons()}
+      {isLoading ? displaySkeleton() : displayRepositories()}
+    </Stack>
   );
 };
 
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,61 +1,62 @@
-import {Stack, Skeleton} from "@mui/material";
-import {useGetUserRepositories} from "../hooks/useGetRepositories.tsx";
-import {useGetUser} from "../hooks/useGetUser.tsx";
+import { Stack, Skeleton } from "@mui/material";
+import { useGetUserRepositories } from "../hooks/useGetRepositories.tsx";
+import { useGetUser } from "../hooks/useGetUser.tsx";
 import UserDetails from "../components/UserDetails.tsx";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import UserRepository from "../components/UserRepository.tsx";
-import {useState, Fragment} from "react";
-import {parseLinkHeader} from "../utils/parseGithubApiLink.ts";
+import { useState, Fragment } from "react";
+import { parseLinkHeader } from "../utils/parseGithubApiLink.ts";
 import PaginationLinks from "../components/PaginationLinks.tsx";
-import {useDocumentTitle} from "../hooks/useDocumentTitle.tsx";
+import { useDocumentTitle } from "../hooks/useDocumentTitle.tsx";
 
 const User = () => {
   const params = useParams();
-  const user = params?.userName ?? ''
-  const address = `https://api.github.com/users/${user}/repos?page=1&per_page=5`
-  const [currentUrl, setCurrentUrl] = useState(address)
-  useDocumentTitle(user)
-  const { isLoading, data} = useGetUserRepositories(currentUrl)
-  const { data: userData } = useGetUser(user)
-  const parsedPaginationLinks = parseLinkHeader(data?.headers ?? '')
+  const user = params?.userName ?? "";
+  const address = `https://api.github.com/users/${user}/repos?page=1&per_page=5`;
+  const [currentUrl, setCurrentUrl] = useState(address);
+  useDocumentTitle(user);
+  const { isLoading, data } = useGetUserRepositories(currentUrl);
+  const { data: userData } = useGetUser(user);
+  const parsedPaginationLinks = parseLinkHeader(data?.headers ?? "");
 
-  const displaySkeleton = () => (
-      new Array(5).map(item =>
-          <Skeleton key={item} variant="rectangular" width={500} height={150} />
-      )
-  )
+  const displaySkeleton = () =>
+    new Array(5).map((item) => (
+      <Skeleton key={item} variant="rectangular" width={500} height={150} />
+    ));
 
   const handlePageChange = (changedUrl: string) => {
-    setCurrentUrl(changedUrl)
-  }
+    setCurrentUrl(changedUrl);
+  };
 
   const displayRepositories = () => {
     return (
-        <Fragment>
-          <UserDetails userName={userData?.login} avatarUrl={userData?.avatar_url} url={userData?.html_url}/>
-          {data?.data.map((repo, index) => (
-              <UserRepository key={index} {...repo} />
-          ))}
-          <PaginationLinks links={parsedPaginationLinks} callback={handlePageChange}/>
-        </Fragment>
-    )
-  }
+      <Fragment>
+        <UserDetails
+          userName={userData?.login}
+          avatarUrl={userData?.avatar_url}
+          url={userData?.html_url}
+        />
+        {data?.data.map((repo, index) => (
+          <UserRepository key={index} {...repo} />
+        ))}
+        <PaginationLinks
+          links={parsedPaginationLinks}
+          callback={handlePageChange}
+        />
+      </Fragment>
+    );
+  };
 
   return (
-      <Stack
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          spacing={0.5}
-          sx={{mt: '5rem'}}
-      >
-        {isLoading && !userData ? (
-            displaySkeleton()
-
-        ) : displayRepositories()
-
-        }
-      </Stack>
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={0.5}
+      sx={{ mt: "5rem" }}
+    >
+      {isLoading && !userData ? displaySkeleton() : displayRepositories()}
+    </Stack>
   );
 };
 
