1cee08b642e262995e4d34026fb2fbde740c7dec
mdmims <mdmims.tech@gmail.com>
2023-05-26 09:29:11 -0400

    refactor pagination styling

--- src/components/PaginationLinks.tsx
+++ src/components/PaginationLinks.tsx
@@ -1,5 +1,9 @@
 import Button from "@mui/material/Button";
-import {ButtonGroup} from "@mui/material";
+import { ButtonGroup } from "@mui/material";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 
 interface KeyValueMap<T> {
   [key: string]: T;
@@ -10,35 +14,53 @@ interface LinksProps {
   callback: (arg: string) => void;
 }
 
-const PaginationLinks = ({links, callback}: LinksProps) => {
-  const prevPage = links?.prev
-  const nextPage = links?.next
-  const lastPage = links?.last
+const PaginationLinks = ({ links, callback }: LinksProps) => {
+  const prevPage = links?.prev;
+  const nextPage = links?.next;
+  const lastPage = links?.last;
+  const firstPage = links?.first;
 
   return (
-      <ButtonGroup sx={{display: "flex", flexDirection: "row", alignItems: "center", mb: "3 rem"}}>
-        {prevPage && (
-            <Button
-                variant="contained"
-                onClick={() => callback(prevPage)}
-            >Previous Page
-            </Button>
-        )}
-        {nextPage && (
-            <Button
-                variant="contained"
-                onClick={() => callback(nextPage)}
-            >Next Page
-            </Button>
-        )}
-        {lastPage && (
-            <Button
-                variant="contained"
-                onClick={() => callback(lastPage)}
-            >Last Page
-            </Button>
-        )}
-      </ButtonGroup>
+    <ButtonGroup
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        mb: "3 rem",
+      }}
+    >
+      <Button
+        variant="contained"
+        onClick={() => callback(firstPage)}
+        disabled={!firstPage}
+      >
+        <FirstPageIcon />
+      </Button>
+
+      <Button
+        variant="contained"
+        onClick={() => callback(prevPage)}
+        disabled={!prevPage}
+      >
+        <NavigateBeforeIcon />
+      </Button>
+
+      <Button
+        variant="contained"
+        onClick={() => callback(nextPage)}
+        disabled={!nextPage}
+      >
+        <NavigateNextIcon />
+      </Button>
+
+      <Button
+        variant="contained"
+        onClick={() => callback(lastPage)}
+        disabled={!lastPage}
+      >
+        <LastPageIcon />
+      </Button>
+    </ButtonGroup>
   );
 };
 
